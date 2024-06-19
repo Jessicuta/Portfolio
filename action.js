8e243fd0c1f6af19296533ae75cbb24e61a904a4
Jessicuta <jessicacodreanu@gmail.com>
2024-06-18 20:25:14 -0400

    add

--- action.js
+++ action.js
@@ -1,22 +1,23 @@
-        let lastScrollTop = 0;
-        const header = document.querySelector('.header');
+// script.js
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
 
-        window.addEventListener("scroll", function() {
-            let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-            if (currentScroll > lastScrollTop) {
-                // Scroll down
-                header.style.top = "-100px"; // Adjust the value as needed
-            } else {
-                // Scroll up
-                header.style.top = "0";
-            }
-            lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-        }, false);
+window.addEventListener("scroll", function() {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScroll > lastScrollTop) {
+        // Scroll down
+        header.style.top = "-100px"; // Adjust the value as needed
+    } else {
+        // Scroll up
+        header.style.top = "0";
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+}, false);
 
-        function hideMenu() {
-            document.getElementById('navLinks').style.display = 'none';
-        }
+function hideMenu() {
+    document.getElementById('navLinks').style.display = 'none';
+}
 
-        function showMenu() {
-            document.getElementById('navLinks').style.display = 'flex';
-        }
+function showMenu() {
+    document.getElementById('navLinks').style.display = 'flex';
+}
