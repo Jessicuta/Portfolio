e482e7868713bc2e90fb5ff79d5e64980e739f2f
Jessicuta <jessicacodreanu@gmail.com>
2024-06-20 13:49:50 -0400

    php

---
+++ send_email.php
@@ -0,0 +1,26 @@
+<?php 
+if ($SERVER ["RESQUEST_METHOD"] == "POST"){
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo "Invalid email format";
+        exit;
+    }
+
+    $to = "codreanu_j@outlook.com";
+    $subject = $name;
+    $headers = "From: " .$email;
+    $body = $message;
+
+    if(mail($to,$subject,$body,$headers)){
+        echo "Email sent successfully";
+    }else {
+        echo "Failed to send email";
+    } 
+} else {
+    echo "Invalid request method";
+}
+
+?>
